1534bca6d737a241d19d3c42a4229549ea1f1644
Jeremy Bini <jeremybini@gmail.com>
2017-09-28 16:11:10 -0400

    Add extra options to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ const distPath = path.join(__dirname, "./dist");
 
 const webpackConfiguration = {
   entry: {
-    app: "./scripts/index.js"
+    app: "./scripts/index.js",
+    vendor: Object.keys(packageJson.dependencies)
   },
   output: {
     filename: "js/[name].js",
@@ -16,26 +17,45 @@ const webpackConfiguration = {
   module: {
     rules: [
       {
-        test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: "babel-loader",
+        test: /\.js$/
       },
       {
         test: /\.scss$/,
         use: [
           { loader: "style-loader" },
           { loader: "css-loader" },
+          { loader: "postcss-loader" },
           { loader: "sass-loader" }
         ]
       }
     ]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      minChunks: Infinity,
+      name: "vendor"
+    }),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "html/index.html"
     })
-  ]
+  ],
+  devServer: {
+    contentBase: distPath,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    host: "0.0.0.0",
+    historyApiFallback: true,
+    inline: true,
+    noInfo: true,
+    port: 4321,
+    publicPath: "/",
+    quiet: false
+  },
+  devtool: "inline-cheap-module-source-map"
 };
 
 module.exports = webpackConfiguration;
